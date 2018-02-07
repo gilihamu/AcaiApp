96cace22e1c19058e8054ec5be091091ee4808d5
Miguel Daipre <migueldaipre@gmail.com>
2018-02-07 12:54:28 -0200

    Get client add other class, FCMRetrofitClient.class

--- app/src/main/java/migueldaipre/com/acaiapp/Remote/RetrofitClient.java
+++ app/src/main/java/migueldaipre/com/acaiapp/Remote/RetrofitClient.java
@@ -1,23 +1,12 @@
 package migueldaipre.com.acaiapp.Remote;
 
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
+
 
 public class RetrofitClient {
 
     private static Retrofit retrofit = null;
-
-    public static Retrofit getClient(String baseURL)
-    {
-        if (retrofit == null)   {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(baseURL)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-        return retrofit;
-    }
 
     public static Retrofit getGoogleClient(String baseURL)
     {
