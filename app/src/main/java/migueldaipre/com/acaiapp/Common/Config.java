f9cbcf134cfca18a9be30d1b344e2746f57b99f4
Miguel Daipre <migueldaipre@gmail.com>
2018-02-08 02:28:40 -0200

    Paypal Client ID changed

--- app/src/main/java/migueldaipre/com/acaiapp/Common/Config.java
+++ app/src/main/java/migueldaipre/com/acaiapp/Common/Config.java
@@ -2,5 +2,5 @@ package migueldaipre.com.acaiapp.Common;
 
 public class Config {
 
-    public static final String PAYPAL_CLIENT_ID = "AcCRuLZNRaCJ1G3nDfiYQBkFGF_1jjBKETXaj65ODxho0XXqAbBsNewHdMXaZkpMQzEBFQK1ejGNZhAh";
+    public static final String PAYPAL_CLIENT_ID = "AZh9YudkuXnQgtEXCxTzNAyWbdT3DUcVEZlSiETh2VaSKOHahbI__VINgs7EHmdzoYvm3odasanBoziK";
 }
