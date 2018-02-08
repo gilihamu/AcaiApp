56f378a93d016f21673dcd6f6a33830bde9a5465
Miguel Daipre <migueldaipre@gmail.com>
2018-02-08 02:30:43 -0200

    Method constructor deleted

--- app/src/main/java/migueldaipre/com/acaiapp/Model/MyResponse.java
+++ app/src/main/java/migueldaipre/com/acaiapp/Model/MyResponse.java
@@ -10,11 +10,4 @@ public class MyResponse {
     public int canonical_ids;
     public List<Result> results;
 
-    public MyResponse(long multicast_id, int success, int failure, int canonical_ids, List<Result> results) {
-        this.multicast_id = multicast_id;
-        this.success = success;
-        this.failure = failure;
-        this.canonical_ids = canonical_ids;
-        this.results = results;
-    }
 }
